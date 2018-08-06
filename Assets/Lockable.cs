017179b92d7893f4bd3b1f53b4325c9a98bb2e61
PancakesMan <so_awesome12@hotmail.com>
2018-08-06 11:39:45 +0930

    added openable door, can be locked

---
+++ Assets/Lockable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lockable : MonoBehaviour {
+
+    public bool Locked = false;
+
+    private void Update()
+    {
+        if (Locked)
+        {
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+        }
+        else { GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None; }
+    }
+}
