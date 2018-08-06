ac943b321e55bbfc426d3e9cfb68f34b0120cf90
PancakesMan <so_awesome12@hotmail.com>
2018-08-06 14:18:26 +0930

    Some Claw Machine Code

---
+++ Assets/ClawMachineController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClawMachineController : MonoBehaviour {
+
+    public bool Active = false;
+    public GameObject FrontBackMover, LeftRightMover, Claw;
+
+    private int _TimesButtonPressed = -1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Active)
+        {
+            if (other.CompareTag("Button"))
+            {
+                _TimesButtonPressed += 1;
+                _TimesButtonPressed %= 3;
+
+                switch (_TimesButtonPressed)
+                {
+                    case 0:
+                        FrontBackMover.GetComponent<PlatformScript>().enabled = true;
+                        break;
+
+                    case 1:
+                        FrontBackMover.GetComponent<PlatformScript>().enabled = false;
+                        LeftRightMover.GetComponent<PlatformScript>().enabled = true;
+                        break;
+
+                    case 2:
+                        LeftRightMover.GetComponent<PlatformScript>().enabled = false;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
