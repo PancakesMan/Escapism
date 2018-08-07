ab97fbce091b13f92110926f9a3a4a040b07261f
PancakesMan <so_awesome12@hotmail.com>
2018-08-07 11:33:13 +0930

    Safe in test scene

--- Assets/Escapism/Scripts/Lockable.cs
+++ Assets/Escapism/Scripts/Lockable.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class Lockable : MonoBehaviour {
 
     public bool Locked = false;
+    public VRTK.Controllables.PhysicsBased.VRTK_PhysicsSlider[] DisabledObjectsWhenUnlocked;
 
     private void Update()
     {
         if (Locked)
         {
             GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+            foreach (VRTK.Controllables.PhysicsBased.VRTK_PhysicsSlider drawer in DisabledObjectsWhenUnlocked)
+                drawer.enabled = true;
         }
-        else { GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None; }
+        else
+        {
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            foreach (VRTK.Controllables.PhysicsBased.VRTK_PhysicsSlider drawer in DisabledObjectsWhenUnlocked)
+                drawer.enabled = false;
+        }
     }
 }
