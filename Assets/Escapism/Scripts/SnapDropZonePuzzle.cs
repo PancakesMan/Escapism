643093b48d2779d8b457335ff23e6c13a16fc7de
Eric Vuglar <s171764@students.aie.edu.au>
2018-08-13 12:28:42 +0930

    SnapDropZonePuzzle script

---
+++ Assets/Escapism/Scripts/SnapDropZonePuzzle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using VRTK;
+
+[System.Serializable]
+public struct DropAreaObjectConnector
+{
+    public VRTK_SnapDropZone  SnapDropZone;
+    public GameObject         Object;
+}
+
+public class SnapDropZonePuzzle : MonoBehaviour {
+
+    public DropAreaObjectConnector[] List;
+
+    [System.Serializable]
+    public class PuzzleCompletedEvent : UnityEvent { }
+
+    public PuzzleCompletedEvent OnPuzzleCompleted;
+
+	// Use this for initialization
+	void Start () {
+		foreach (DropAreaObjectConnector connector in List)
+        {
+            connector.SnapDropZone.ObjectEnteredSnapDropZone += ObjectSnappedHandler;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ObjectSnappedHandler(object sender, SnapDropZoneEventArgs e) {
+        foreach (DropAreaObjectConnector connector in List)
+        {
+            if (connector.SnapDropZone.GetCurrentSnappedObject().name != connector.Object.name)
+                break;
+
+            OnPuzzleCompleted.Invoke();
+        }
+    }
+}
