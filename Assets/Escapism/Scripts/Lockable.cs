004e4dc6ef1e9aac248132c413100d32b4729b90
Eric Vuglar <s171764@students.aie.edu.au>
2018-08-14 14:26:43 +0930

    Changed Lockable to work for Interactable Objects

--- Assets/Escapism/Scripts/Lockable.cs
+++ Assets/Escapism/Scripts/Lockable.cs
@@ -2,22 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum LockType { Rigidbody, Interactable }
+
 public class Lockable : MonoBehaviour {
 
+    public LockType Type = LockType.Rigidbody;
     public bool Locked = false;
+
     public VRTK.Controllables.PhysicsBased.VRTK_PhysicsSlider[] DisabledObjectsWhenUnlocked;
 
     private void Update()
     {
         if (Locked)
         {
-            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            switch (Type)
+            {
+                case LockType.Rigidbody:
+                    GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+                    break;
+                case LockType.Interactable:
+                    GetComponent<VRTK.VRTK_InteractableObject>().isGrabbable = false;
+                    break;
+                default:
+                    break;
+            }
             foreach (VRTK.Controllables.PhysicsBased.VRTK_PhysicsSlider drawer in DisabledObjectsWhenUnlocked)
                 drawer.enabled = true;
         }
         else
         {
-            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            switch (Type)
+            {
+                case LockType.Rigidbody:
+                    GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+                    break;
+                case LockType.Interactable:
+                    GetComponent<VRTK.VRTK_InteractableObject>().isGrabbable = true;
+                    break;
+                default:
+                    break;
+            }
             foreach (VRTK.Controllables.PhysicsBased.VRTK_PhysicsSlider drawer in DisabledObjectsWhenUnlocked)
                 drawer.enabled = false;
         }
