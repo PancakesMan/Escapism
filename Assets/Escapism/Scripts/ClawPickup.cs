ebf77ae70e8a1bc50f7841a4b40d03aed50716e1
Eric Vuglar <s171764@students.aie.edu.au>
2018-08-20 15:17:55 +0930

    ClawMachineController Script Changes

--- Assets/Escapism/Scripts/ClawPickup.cs
+++ Assets/Escapism/Scripts/ClawPickup.cs
@@ -4,17 +4,6 @@ using UnityEngine;
 using VRTK;
 
 public class ClawPickup : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Claw hit something");
