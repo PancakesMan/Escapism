cc4f29f9a48d2bc3f67c99b6a890ad942fa6b453
Eric Vuglar <s171764@students.aie.edu.au>
2018-08-21 13:03:08 +0930

    TriggedByObject and MultistagePuzzle scripts

---
+++ Assets/Escapism/Scripts/MultistagePuzzle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MultistagePuzzle : MonoBehaviour {
+    [System.Serializable]
+    public class PuzzleCompleted : UnityEvent { }
+
+    public GameObject[] Objects;
+    public PuzzleCompleted OnPuzzleCompleted;
+
+	// Use this for initialization
+	public void CheckObjectsEnabled()
+    {
+        foreach (GameObject go in Objects)
+            if (!go.activeSelf)
+                return;
+
+        OnPuzzleCompleted.Invoke();
+    }
+}
---
+++ Assets/Escapism/Scripts/TriggeredByObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggeredByObject : MonoBehaviour {
+
+    [System.Serializable]
+    public class TriggerEnter : UnityEvent { }
+    [System.Serializable]
+    public class TriggerExit : UnityEvent { }
+
+    public GameObject DetectTriggeredBy;
+
+    public TriggerEnter OnTriggerEntered;
+    public TriggerExit OnTriggerExited;
+
+    // Use this for initialization
+    private void OnTriggerEnter(Collider other)
+    {
+        if (enabled && other.gameObject == DetectTriggeredBy) OnTriggerEntered.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (enabled && other.gameObject == DetectTriggeredBy) OnTriggerExited.Invoke();
+    }
+}
