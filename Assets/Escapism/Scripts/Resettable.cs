5326fa2830fe3fc7b18b1e76d6e4eca819060326
Eric Vuglar <s171764@students.aie.edu.au>
2018-08-27 14:53:46 +0930

    Made variables public

--- Assets/Escapism/Scripts/Resettable.cs
+++ Assets/Escapism/Scripts/Resettable.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class Resettable : MonoBehaviour {
 
-    Vector3 _Position;
-    Quaternion _Rotation;
+    public Vector3 Position;
+    public Vector3 Rotation;
 
 	// Use this for initialization
 	void Start () {
-        _Position = transform.position;
-        _Rotation = transform.rotation;
+        //_Position = transform.position;
+        //_Rotation = transform.rotation;
 	}
 
     public void Reset()
     {
-        transform.position = _Position;
-        transform.rotation = _Rotation;
+        transform.position = Position;
+        transform.rotation = Quaternion.Euler(Rotation);
     }
 }
