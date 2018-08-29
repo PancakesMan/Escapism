41e2df663abb380a4e9c6cf285cdfad1ba7b3f0a
Eric Vuglar <s171764@students.aie.edu.au>
2018-08-29 10:05:48 +0930

    added and made pig work

---
+++ Assets/Escapism/Scripts/JointBreakEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class JointBreakEvent : MonoBehaviour {
+
+    [System.Serializable]
+    public class JointBreakingEvent : UnityEvent { }
+
+    public JointBreakingEvent OnJointBreaking;
+
+    private void OnJointBreak(float breakForce)
+    {
+        OnJointBreaking.Invoke();
+    }
+}
