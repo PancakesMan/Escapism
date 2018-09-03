1bbb127e4b1779d269e522f5c4ffd3e9d811fb0c
Eric Vuglar <s171764@students.aie.edu.au>
2018-09-03 13:42:27 +0930

    Script Changes
    Reset time for resettable objects

--- Assets/Escapism/Scripts/Resettable.cs
+++ Assets/Escapism/Scripts/Resettable.cs
@@ -7,13 +7,20 @@ public class Resettable : MonoBehaviour {
     public Vector3 Position;
     public Vector3 Rotation;
 
+    public float ResetTime = 0.0f;
+
 	// Use this for initialization
 	void Start () {
         //_Position = transform.position;
         //_Rotation = transform.rotation;
 	}
 
-    public void Reset()
+    public void StartReset()
+    {
+        Invoke("InstantReset", ResetTime);
+    }
+
+    public void InstantReset()
     {
         transform.position = Position;
         transform.rotation = Quaternion.Euler(Rotation);
