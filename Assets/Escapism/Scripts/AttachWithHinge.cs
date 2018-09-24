b755a63a7beac5c17834c7d25aa385bde8184512
Eric Vuglar <s171764@students.aie.edu.au>
2018-09-24 15:54:11 +0930

    Wall thingo

---
+++ Assets/Escapism/Scripts/AttachWithHinge.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachWithHinge : MonoBehaviour {
+
+    private List<GameObject> AttachedPreviously;
+
+	// Use this for initialization
+	void Start () {
+        AttachedPreviously = new List<GameObject>();
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.GetComponent<AttachWithHinge>() ||
+            AttachedPreviously.Contains(collision.gameObject)) return;
+
+        Rigidbody body = collision.rigidbody;
+        if (!body)
+            body = collision.gameObject.AddComponent<Rigidbody>();
+
+        FixedJoint joint = gameObject.AddComponent<FixedJoint>();
+        joint.breakForce = 50;
+        joint.connectedBody = body;
+
+        AttachedPreviously.Add(collision.gameObject);
+    }
+}
