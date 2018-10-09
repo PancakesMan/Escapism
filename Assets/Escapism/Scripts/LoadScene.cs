26493424cdbd9e25670fbe41aaab1a41b78da636
Eric Vuglar <s171764@students.aie.edu.au>
2018-10-09 13:04:40 +1030

    LoadScene and Timer scripts

---
+++ Assets/Escapism/Scripts/LoadScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+    public int sceneBuildIndex;
+
+    public void Load()
+    {
+        SceneManager.LoadScene(sceneBuildIndex);
+    }
+
+    public void LoadAsync()
+    {
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    private IEnumerator LoadSceneAsync()
+    {
+        AsyncOperation load = SceneManager.LoadSceneAsync(sceneBuildIndex);
+        while (!load.isDone)
+            yield return null;
+    }
+}
---
+++ Assets/Escapism/Scripts/Timer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour {
+
+    [System.Serializable]
+    public class TimerCompletedEvent : UnityEvent { }
+
+    public float TimerLength;
+    public TimerCompletedEvent OnTimerCompleted;
+
+    private float _timer;
+    private bool started;
+
+	// Update is called once per frame
+	void Update () {
+        if (started)
+        {
+            _timer += Time.deltaTime;
+            if (_timer > TimerLength)
+            {
+                ResetTimer();
+                OnTimerCompleted.Invoke();
+            }
+        }
+	}
+
+    public void StartTimer()
+    {
+        started = true;
+    }
+
+    public void PauseTimer()
+    {
+        started = false;
+    }
+
+    public void ResetTimer()
+    {
+        started = false;
+        _timer = 0.0f;
+    }
+
+    public float GetTime()
+    {
+        return _timer;
+    }
+}
