9ee6cb40863cabaf3ded55fb8d4295f14f54f8b8
PancakesMan <so_awesome12@hotmail.com>
2018-10-23 13:49:00 +1030

    New Script

---
+++ Assets/Escapism/Scripts/CollisionFadeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionFadeController : MonoBehaviour {
+
+    public VRTK.VRTK_HeadsetCollisionFade Fader;
+
+	public void SetFadeTime(float time)
+    {
+        Fader.blinkTransitionSpeed = time;
+    }
+}
