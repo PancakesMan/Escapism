dc0f4cdb6b36b085e02d6489342efffe4121844f
Eric Vuglar <s171764@students.aie.edu.au>
2018-10-30 13:50:23 +1030

    LevelStatus Script for saving

---
+++ Assets/Escapism/Scripts/LevelStatus.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LevelStatus : MonoBehaviour {
+
+    [System.Serializable]
+    public class LevelCompletedEvent : UnityEvent { }
+
+    public string LevelName;
+    private bool _Completed;
+
+    public LevelCompletedEvent OnLevelCompleted;
+
+	// Use this for initialization
+	void Start () {
+        _Completed = PlayerPrefs.GetInt(LevelName + "Completed") == 1 ? true : false;
+        if (_Completed)
+            OnLevelCompleted.Invoke();
+	}
+
+    void SetComplete()
+    {
+        if (_Completed) return;
+
+        PlayerPrefs.SetInt(LevelName + "Completed", 1);
+        OnLevelCompleted.Invoke();
+    }
+
+    bool GetCompleted()
+    {
+        return _Completed;
+    }
+}
