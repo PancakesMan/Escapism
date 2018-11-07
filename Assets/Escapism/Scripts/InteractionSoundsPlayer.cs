10c53ea69976ac7e1291ae50558582984db106ca
Eric Vuglar <s171764@students.aie.edu.au>
2018-11-07 11:22:37 +1030

    basic script to play sound when the object is grabbed

---
+++ Assets/Escapism/Scripts/InteractionSoundsPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+[RequireComponent(typeof(AudioSource), typeof(VRTK_InteractableObject)]
+public class InteractionSoundsPlayer : MonoBehaviour {
+
+    public AudioClip grabbedAudioClip;
+
+    private VRTK_InteractableObject interactions;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        interactions = GetComponent<VRTK_InteractableObject>();
+        interactions.InteractableObjectGrabbed += Interactions_InteractableObjectGrabbed;
+
+        audioSource = GetComponent<AudioSource>();
+	}
+
+    private void Interactions_InteractableObjectGrabbed(object sender, InteractableObjectEventArgs e)
+    {
+        audioSource.PlayOneShot(grabbedAudioClip);
+    }
+}
