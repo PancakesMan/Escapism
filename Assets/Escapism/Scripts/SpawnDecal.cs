a2b93cc4eab1104a9b1c4cc6341789327bc25e88
Eric Vuglar <s171764@students.aie.edu.au>
2018-11-13 11:56:52 +1030

    Started Spawn Decal script, might work might not who knows needs testing and probably more work

---
+++ Assets/Escapism/Scripts/SpawnDecal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnDecal : MonoBehaviour {
+
+    public Canvas worldspaceCanvas;
+    public Image decal;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Bullet"))
+        {
+            Image newDecal = Instantiate(decal);
+            newDecal.transform.position = collision.contacts[0].point;
+        }
+    }
+}
